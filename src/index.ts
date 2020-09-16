dc26d3993bd9803938b33dc7ffc72da93707c8e2
doojin <dmitry.papka@gmail.com>
2020-09-16 14:08:52 -0500

    the "Hello, world" application

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(PORT, () => console.log(`App started: http://localhost:${PORT}`))
