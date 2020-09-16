0411cf6ead7498244befa86ba5508ec7561684b9
doojin <dmitry.papka@gmail.com>
2020-09-16 14:20:24 -0500

    moving HTTP request handler to a separate folder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import helloWorldHandler from './handler/hello-world'
 
 const PORT = 3000
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.get('/', helloWorldHandler)
 
 app.listen(PORT, () => console.log(`App started: http://localhost:${PORT}`))
