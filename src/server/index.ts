51d01c01e53977452e467e8958916232f6d7eb7d
doojin <dmitry.papka@gmail.com>
2020-09-21 11:36:21 -0500

    testing hook

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,4 +7,9 @@ const app = express()
 
 app.get('/earthquakes', handleErrors(earthquakes))
 
+// TODO: for testing purposes, remove this later!
+app.get('/', (req, res) => {
+  res.json({ message: 'hello, world 1' })
+})
+
 app.listen(PORT, () => console.log(`App started: http://localhost:${PORT}/earthquakes`))
