bbe8917187bd84b56f5498716122ad207fb3de66
doojin <dmitry.papka@gmail.com>
2020-09-21 11:39:04 -0500

    remove unused endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,9 +7,4 @@ const app = express()
 
 app.get('/earthquakes', handleErrors(earthquakes))
 
-// TODO: for testing purposes, remove this later!
-app.get('/', (req, res) => {
-  res.json({ message: 'hello, world 1' })
-})
-
 app.listen(PORT, () => console.log(`App started: http://localhost:${PORT}/earthquakes`))
