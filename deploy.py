b16cdba8ee4bef2efc62222c1e7e225daa57b53e
doojin <dmitry.papka@gmail.com>
2020-12-09 12:31:39 -0300

    updating deployment script

--- deploy.py
+++ deploy.py
@@ -20,12 +20,12 @@ def run_command(description, command, **kwargs):
     print('done!\n')
 
 
+run_command('stopping the server', 'npm stop')
 run_command('pulling latest changes', 'git pull')
+run_command('installing backend dependencies', 'npm install')
 run_command('cleaning frontend directory', 'rm -rf frontend')
 run_command('cloning frontend project', 'git clone %s frontend' % FRONTEND_REPO)
 run_command('installing frontend dependencies', 'npm install', dir='./frontend')
 run_command('compiling frontend project', 'npm run build', dir='./frontend')
 run_command('compiling backend project', 'npm run build')
-run_command('stopping the server', 'npm stop')
 run_command('starting the server', 'NODE_ENV=production npm start')
-
