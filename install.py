aff4aff539b815bb0b7d45298ce871f00590c7ba
doojin <dmitry.papka@gmail.com>
2020-12-09 12:19:29 -0300

    stopping the server before restarting it

--- install.py
+++ install.py
@@ -26,5 +26,6 @@ run_command('cloning frontend project', 'git clone %s frontend' % FRONTEND_REPO)
 run_command('installing frontend dependencies', 'npm install', dir='./frontend')
 run_command('compiling frontend project', 'npm run build', dir='./frontend')
 run_command('compiling backend project', 'npm run build')
+run_command('stopping the server', 'npm stop')
 run_command('starting the server', 'NODE_ENV=production npm start')
 
