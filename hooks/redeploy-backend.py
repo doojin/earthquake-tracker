acfbe34c44c1e32073f42e4677bba55b0e9ade7e
doojin <dmitry.papka@gmail.com>
2020-12-09 15:37:43 -0300

    updating backend deployment script

--- hooks/redeploy-backend.py
+++ hooks/redeploy-backend.py
@@ -3,8 +3,8 @@
 import os
 
 os.chdir('../')
-os.system('npm stop')
 os.system('git pull')
 os.system('npm ci')
 os.system('npm run build')
+os.system('npm stop')
 os.system('NODE_ENV=production npm start')
